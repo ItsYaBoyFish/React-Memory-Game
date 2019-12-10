d0a4a7afb52317866c00918340406fda97c5a313
Austin Graves <austingraves106@aol.com>
2019-12-10 14:55:18 -0600

    I have the squares loading into the application. I still need to work on updating the state for when they have and haven't been clicked. I still need to add the navar information. I also need to strategize how to reshuffle the squares around after click

--- react-memory-game/src/components/GameContainer.js
+++ react-memory-game/src/components/GameContainer.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import MemorySquare from "./MemorySquare";
+import css from "../GameContainer.css"
+class GameContainer extends Component {
+  evaluatePosition = () => {
+    return Math.floor(Math.random() * 800);
+  }
 
-export class GameContainer extends Component {
+  state = {
+    squares: [
+      {hex: "#D3D3D3", position: this.evaluatePosition()},
+      {hex: "#D0D0D0", position: this.evaluatePosition()},
+      {hex: "#C8C8C8", position: this.evaluatePosition()},
+      {hex: "#C0C0C0", position: this.evaluatePosition()},
+      {hex: "#BEBEBE", position: this.evaluatePosition()},
+      {hex: "#B8B8B8", position: this.evaluatePosition()},
+      {hex: "#B0B0B0", position: this.evaluatePosition()},
+      {hex: "#A9A9A9", position: this.evaluatePosition()},
+      {hex: "#A8A8A8", position: this.evaluatePosition()},
+      {hex: "#A0A0A0", position: this.evaluatePosition()},
+      {hex: "#989898", position: this.evaluatePosition()},
+      {hex: "#909090", position: this.evaluatePosition()},
+    ]
+  }
+
   render() {
     return (
-      <div>
-        <MemorySquare />
-        <MemorySquare />
-        <MemorySquare />
-        <MemorySquare />
-
+      <div className="container">
+        {this.state.squares.sort((a, b) => {return a.position - b.position}).map((square, index) => (
+          <MemorySquare key={index} position={square.position} squareColor={square.hex}/>
+        ))}
+        {/* {this.state.squares.sort((a,b) => {return a > b}))
+        .map((square, index) => (
+          <MemorySquare key={index} position={square.position} squareColor={square.hex}/>
+        ))}; */}
       </div>
     )
   }
 };
+
+
 
 export default GameContainer;
--- react-memory-game/src/components/MemorySquare.js
+++ react-memory-game/src/components/MemorySquare.js
@@ -1,34 +1,42 @@
 import React, { Component } from 'react'
 
-export class MemorySquare extends Component {
+class MemorySquare extends Component {
+
+  state = {
+    color: this.props.squareColor,
+    clicked: false,
+    position: this.props.position
+  }
+
   render() {
+    const styles = {
+      coloredSquare: {
+        // marginLeft: "auto",
+        // marginRight: "auto",
+        marginTop: "7.5px",
+        height: "180px",
+        width: "180px",
+        background: this.state.color,
+        borderRadius: "10px",
+      },
+      container: {
+        // // margin: "10px",
+        // textAlign: "center",
+        // height: "195px",
+        // width: "195px",
+        // border: `solid 1px ${this.state.color}`,
+        // borderRadius: "10px"
+      }
+     
+    }
     return (
       <div style={styles.container}>
-      <div style={styles.coloredSquare}></div>
+       <div style={styles.coloredSquare}></div>
       </div>
     )
   }
 }
 
-const styles = {
-  coloredSquare: {
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginTop: "7.5px",
-    height: "180px",
-    width: "180px",
-    background: "lightgrey",
-    borderRadius: "10px",
-  },
-  container: {
-    margin: "10px",
-    textAlign: "center",
-    height: "195px",
-    width: "195px",
-    border: "solid 1px grey",
-    borderRadius: "10px"
-  }
- 
-}
+
 
 export default MemorySquare;
