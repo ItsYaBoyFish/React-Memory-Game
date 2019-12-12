bf259d95f36f4e085f4d6c93cbd3f8c22c97a08f
Austin Graves <austingraves106@aol.com>
2019-12-12 13:42:45 -0600

    Removed the build from the .gitignore file

---
+++ react-memory-game/build/precache-manifest.c2f230b9917414c57d48fd2c698fc259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2229012c48791dfffde08e3701d643",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b24de33f4337aaf4bcd9",
+    "url": "/static/css/main.cf2f7148.chunk.css"
+  },
+  {
+    "revision": "2506a83f288240c0c396",
+    "url": "/static/js/2.f620143e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f620143e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b24de33f4337aaf4bcd9",
+    "url": "/static/js/main.74ff1c88.chunk.js"
+  },
+  {
+    "revision": "99ba2ceb5603b9ca8240",
+    "url": "/static/js/runtime-main.b78d3fe5.js"
+  }
+]);
